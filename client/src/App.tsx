d202f46f806f5b1278c390d7d014985c5522ef04
astatsuya <sharekobekko@gmail.com>
2021-03-06 14:48:34 +0900

    create top form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Top } from "./pages/Top";
 
 export const App: React.VFC = () => {
+  const [username, setUsername] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+  };
   return (
     <div className="App">
-      <Top />
+      <Top
+        username={username}
+        setUsername={setUsername}
+        roomName={roomName}
+        setRoomName={setRoomName}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 };
--- client/src/pages/Top/index.tsx
+++ client/src/pages/Top/index.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import "./styles.css";
 
-export const Top: React.VFC = () => {
-  return <div className="top-wrapper">top</div>;
+type Props = {
+  username: string;
+  setUsername: (e: string) => void;
+  roomName: string;
+  setRoomName: (e: string) => void;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export const Top: React.VFC<Props> = ({
+  username,
+  setUsername,
+  roomName,
+  setRoomName,
+  onSubmit,
+}) => {
+  return (
+    <div className="top-wrapper">
+      <form onSubmit={onSubmit}>
+        <div className="text-input">
+          <label>
+            <span>username</span>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <div className="text-input">
+          <label>
+            <span>roomName</span>
+            <input
+              type="text"
+              value={roomName}
+              onChange={(e) => setRoomName(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <button>Start neu!</button>
+      </form>
+    </div>
+  );
 };
